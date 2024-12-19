af865437c9552a3a9949a1dfb04350405b82d46d
Thahsan Mohomed <mthahzan.developer@gmail.com>
2024-12-19 07:23:53 +0530

    chore: code cleanup
    - Lint and Test fixes

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   presets: [
-    ['module:react-native-builder-bob/babel-preset', { modules: 'commonjs' }],
+    ['module:react-native-builder-bob/babel-preset', {modules: 'commonjs'}],
   ],
 };
